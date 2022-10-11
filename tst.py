45ff9d5daa66735b5f85c0680f0fd433e6b69ed7
RSAvila <115040125+RSAvila@users.noreply.github.com>
2022-10-11 16:18:12 -0300

    Add files via upload

--- tst.py
+++ tst.py
@@ -1,8 +1,49 @@
-tst = {1: "12345678",
+'''tst = {1: "12345678",
  2: "87654321"}
 
 tst2 = {1: 2412356,
     2: 2345613
 }
 
-print(tst2.keys(2412356))
+print(tst2.keys(2412356))'''
+import ast
+
+
+def note():
+    print("Função note()")
+    with open('UserList.txt', 'r+') as login:
+        try:
+            dicionario = ast.literal_eval(login.read())
+            tamanho = len(list(dicionario.keys()))
+            proximoLog = tamanho+1
+            if dicionario == '' or dicionario == None:
+                login.write('{1: "123456","12345678"}')
+            else:
+                login.write(proximoLog)
+        except:
+            print("Não foi possivel realizar a conversão")
+        
+        
+        
+
+        
+def tst():
+    print("Função tst()")
+    with open('UserList.txt','r+') as login:
+        dicionario = login.read()
+        print(type(dicionario))
+        print(dicionario)
+
+#tst()
+                
+
+note()
+
+
+def get():
+    print("Função get()")
+    with open('UserList.txt', 'r') as login:
+        dicionario = login.read()
+        print(dicionario)
+
+#get()
